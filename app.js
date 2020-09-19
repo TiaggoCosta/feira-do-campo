1753186015bf05fec27ecd1e32148aa916419e98
Karolina <karolina_cris@hotmail.com>
2020-09-19 17:16:13 -0300

    start using express layouts and adding route to admin products

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+const adminProductsRouter = require('./routes/admin/produtos');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs') ;
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded());
+
 app.use(express.static(__dirname + '/public'));
+app.use(adminProductsRouter);
 
 app.get('/', (req, res) => {
-    res.render('home');
-});
+    res.render('pages/home');
+})
 
 app.listen(3000, () => {
     console.log('A Feira do Campo está no ar!');
