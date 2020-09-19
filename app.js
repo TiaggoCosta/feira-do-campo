9af6b67494e9845096a873ce8f06fe6a88cda5f4
Tiago Costa <tiago.tagcosta@gmail.com>
2020-09-19 16:40:51 -0300

    add DB connection

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+require("dotenv").config();
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    process.env.DATABASEURL,
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
+    (error) => {
+        if(error) console.log(error);
+
+        console.log('Aplicação conectada ao banco de dados!');
+    }
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
@@ -10,6 +23,6 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log('A Feira do Campo está no ar!');
 });
