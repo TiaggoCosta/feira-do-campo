2b5100340323cb28bc39c05f63c39ca20605b81e
Tiago Costa <tiago.tagcosta@gmail.com>
2020-09-22 22:18:29 -0300

    add delete product route

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -3,20 +3,20 @@ const router = express.Router();
 const Product = require('../../models/product');
 
 // INDEX - GET all products
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
   Product.find({}, (err, products) => {
     if(err){
-        console.log(err);
-        res.redirect('/');
+      console.log(err);
+      res.redirect('/');
     } else {
-       res.render('pages/admin/products',{ products });
+      res.render('pages/products/index', { products });
     }
   });
 });
 
 // NEW - GET to show view for adding new product
-router.get('/new', async (req, res) => {
-  res.send('pages/admin/products/new');
+router.get('/new', (req, res) => {
+  res.render('pages/products/new');
 });
 
 // CREATE - add new product to DB
@@ -24,41 +24,40 @@ router.post('/', (req, res) => {
   const { title, price, image } = req.body;
   const newProduct = { title, price, image };
   Product.create(newProduct, (err, createdProduct) => {
-      if(err) {
-          console.log(err);
-      } else {
-          res.redirect('/admin/products/');
-      }
+    if(err) {
+      console.log(err);
+    }
+    res.redirect('/admin/products/');
   });
 });
 
 // EDIT - GET to show view for editing a product
-router.get('/:id/edit', async (req, res) => {
+router.get('/:id/edit', (req, res) => {
   Product.findById(req.params.id, (err, foundProduct) => {
-      if(err) {
-          console.log(err);
-          res.redirect('/admin/products/');
-      } else {
-          res.render('pages/admin/edit', { product: foundProduct });
-      }
+    res.render('pages/products/edit', { product: foundProduct });
   });
 });
 
 // UPDATE - update some product
 router.put('/:id', (req, res) => {
-  Product.findByIdAndUpdate(req.params.id, req.body.product, (err, updatedProduct) => {
-      if(err) {
-          console.log(err);
-          res.redirect('/admin/products/');
-      } else {
-          res.redirect('/admin/products/' + req.params.id);
-      }
+  const { title, price, image } = req.body;
+  const product = { title, price, image };
+  Product.findByIdAndUpdate(req.params.id, product, (err, updatedProduct) => {
+    if(err) {
+      console.log(err);
+    } 
+    res.redirect('/admin/products/');
   });
 });
 
 // DELETE the product with received id
-router.delete('/:id', async (req, res) => {
-  res.send("Implementar tratamento para deletar produto");
+router.delete('/:id', (req, res) => {
+  Product.findByIdAndRemove(req.params.id, (err) => {
+    if(err) {
+      console.log(err);
+    } 
+    res.redirect('/admin/products/');
+  });
 });
 
 module.exports = router;
