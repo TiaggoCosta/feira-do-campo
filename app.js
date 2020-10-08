68f40785d27eaf1f87640f2818d6481d573a5aed
Tiago Costa <tiago.tagcosta@gmail.com>
2020-10-08 19:02:26 -0300

    Add cookie-session

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 require("dotenv").config();
 
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
-const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const multer = require('multer');
-const upload = multer();
 const indexRoutes = require('./routes/index');
 const adminProductsRoutes = require('./routes/admin/products');
+const cartsRoutes = require('./routes/carts');
+
+const multer = require('multer');
+const upload = multer();
+const cookieSession = require('cookie-session');
 //const seedDB = require("./seeds");
 
 app.set('view engine', 'ejs') ;
@@ -18,6 +21,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array()); 
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride("_method"));
+app.use(
+    cookieSession({
+      keys: ['digaamigoeentre']
+    })
+  );
 
 mongoose.connect(
     process.env.DATABASEURL,
@@ -32,6 +40,7 @@ mongoose.connect(
 
 app.use('/', indexRoutes);
 app.use('/admin/products', adminProductsRoutes);
+app.use('/cart', cartsRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log('A Feira do Campo está no ar!');
