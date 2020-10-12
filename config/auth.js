2e2ad4d1739945ddb3e607bf13904ffeb21d1832
Nadine Schneider <nadine_schneider@hotmail.com>
2020-10-12 01:00:43 -0300

    #159-MetodoAutenticacao

---
+++ config/auth.js
@@ -0,0 +1,41 @@
+const localStrategy = require("passport-local").Strategy
+const mongoose = require("mongoose");
+
+//verficar
+const bcrypt = require("bcryptjs");
+
+const User = require('../models/user');
+
+module.exports = function(passport){
+    console.log('Chegou no auth');
+    passport.use(new localStrategy({usernameField: 'email'}, (email, senha, done) => {
+
+        console.log(email);
+        console.log(senha);
+
+        User.findOne({email: email}).then((user) => {
+            if(!user){
+             return done(null, false, {message: "Esta conta não existe"});
+            }
+
+            //console.log(user);
+            if(senha == user.password){
+                console.log("Login realizado com sucesso!");
+                return done(null, user);
+            } else {
+                return done (null, false, {message: "A senha está incorreta"});
+            }
+        })
+    }))
+
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id)
+    })
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => {
+            done(err, user)
+        })
+    })
+}
--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router  = express.Router();
 const User = require('../models/user');
+const passport = require('passport');
 
 // INDEX - GET login form
 router.get("/", function(req, res){
     res.render("pages/login"); 
   });
 
+router.post("/", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true
+  })(req, res, next)
+})
+    
 module.exports = router;
