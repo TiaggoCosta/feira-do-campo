5126f50d80f66f3199ebca78f6a8f5d8cddb1a15
Tiago Costa <tiago.tagcosta@gmail.com>
2020-10-29 16:50:43 -0300

    Add order routes

---
+++ routes/order.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const orderRepo = require('../models/order');
+const productsRepo = require('../models/product');
+
+const router = express.Router();
+
+// Receive a post request to create new order
+router.post('/', async (req, res) => {
+    const { order } = req.body;
+
+    orderRepo.create(order, (err, createdOrder) => {
+        if(err) {
+            console.log(err);
+        }
+    });  
+
+    res.redirect('/');
+});
+
+module.exports = router;
