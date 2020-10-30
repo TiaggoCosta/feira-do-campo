98c8870ee6c3005b5078310b2599918aaeab5fef
Tiago Costa <tiago.tagcosta@gmail.com>
2020-10-30 16:48:54 -0300

    Add order schema

---
+++ models/order.js
@@ -0,0 +1,13 @@
+const OrderSchema = new mongoose.Schema({
+   costumer: String,
+   producer: String,
+   items: [
+      {
+         productId: String,
+         quantity: Number
+      }
+   ],
+   createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
